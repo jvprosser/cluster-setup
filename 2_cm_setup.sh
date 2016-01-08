b51b07301b447912743797ffe55635b613c778af
Joe Prosser <jvprosser@gmail.com>
2016-01-08 11:35:54 -0500

    Update 2_cm_setup.sh

--- 2_cm_setup.sh
+++ 2_cm_setup.sh
@@ -2,9 +2,13 @@
 
 
 pscp --timeout=0 -h  hostlist.txt -l ec2-user  -X '-oStrictHostKeyChecking=no'  2_target_setup.sh /tmp/2_target_setup.sh
-pscp --timeout=0 -h  hostlist.txt -l ec2-user  -X '-oStrictHostKeyChecking=no'  etc.hosts /tmp/etc.hosts
-pscp --timeout=0 -h  hostlist.txt -l ec2-user  -X '-oStrictHostKeyChecking=no'  jdk-7u51-linux-x64.rpm  /tmp/
 
-pssh --timeout=0 --inline-stdout  -t 0 -h  hostlist.txt -l ec2-user -x '-tt' -X '-oStrictHostKeyChecking=no'  'sudo bash -x -c "/tmp/2_target_setup.sh"'
+jpssh 'wget https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/cloudera-manager.repo;mv cloudera-manager.repo /etc/yum.repos.d/'
+jpssh yum -y install oracle-j2sdk1.7
+jpssh ln -s /usr/java/jdk1.7.0_67-cloudera/ /usr/java/latest
+jpscp mysql-connector-java-5.1.38/mysql-connector-java-5.1.38-bin.jar /tmp
+jpssh mkdir -p /usr/share/java
+jpssh cp /tmp/mysql-connector-java-5.1.38-bin.jar /usr/share/java/mysql-connector-java.jar
+jpssh  'bash -x -c "/tmp/2_target_setup.sh"'
 
 
